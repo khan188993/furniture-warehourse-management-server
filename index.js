18706365eecc4e49fda6a788ca429edd47ea46f5
Md Arfan Khan <khan188993@gmail.com>
2022-05-08 00:25:26 +0600

    3)get and post api for products added

--- index.js
+++ index.js
@@ -14,9 +14,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run(){
+    //DATABASE CONNECTED
     await client.connect();
-    console.log('connected');
     const furnitureCollection = client.db("FurnitureInventoryManagement").collection("Furniture");
+
+    //GET ALL FURNITURE DATA SUCCESSFULLY
+    app.get('/furniture',async (req,res)=>{
+        const query = req.query;
+        const result = await furnitureCollection.find(query).toArray();
+        console.log(result);
+        res.send(result);
+    })
+    
+    //POST FURNITURE DATA SUCCESSFULLY
+    app.post('/furniture',async (req,res)=>{
+        const newFurniture = req.body;
+        console.log(newFurniture);
+        const result = await furnitureCollection.insertOne(newFurniture);
+        res.send("new added");
+    })
+
 }
 run().catch(console.dir('err'));
 
