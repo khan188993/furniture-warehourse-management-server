b8bdf74365003320f8b95937ab2320820c721735
Md Arfan Khan <khan188993@gmail.com>
2022-05-08 00:09:23 +0600

    2)server connected with database

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+//middleware use 
+app.use(cors());
+app.use(express.json());
+
+//DATABASE Connection Setup 
+const uri = "mongodb+srv://arfankhan:AmKj8HzLIQ7BKQ4R@cluster0.dzplv.mongodb.net/FurnitureInventoryManagement?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    await client.connect();
+    console.log('connected');
+    const furnitureCollection = client.db("FurnitureInventoryManagement").collection("Furniture");
+}
+run().catch(console.dir('err'));
+
 
 app.get('/',(req,res)=>{
 
